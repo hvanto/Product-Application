14b26a9217b8f5bf88f779f8be1e38512f2b3bc8
Henry van Toledo <113152922+HenryVanToledo@users.noreply.github.com>
2024-06-01 19:51:22 +1000

    express

---
+++ react/src/apollo/client.js
@@ -0,0 +1,20 @@
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const GRAPHQL_ENDPOINT = "ws://localhost:4000/graphql";
+
+const link = new GraphQLWsLink(
+  createClient({
+      url: GRAPHQL_ENDPOINT,
+  })
+);
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
+
+export default client;
