6469567ab336fb3b814358d412f9e034755fbb35
hvanto <hvanto19@gmail.com>
2024-06-02 16:48:50 +1000

    Updates

--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const { ApolloServer, gql } = require("apollo-server-express");
+const cors = require("cors");
 const db = require("./src/database/index.js");
 
-// type definitions and resolvers for GraphQL
 const typeDefs = gql`
   type User {
     userId: ID!
@@ -23,13 +22,11 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     users: async () => {
-      // Implement database fetching logic
       return await db.User.findAll();
     },
   },
   Mutation: {
     blockUnblockUser: async (_, { userId, block }) => {
-      // Implement database update logic
       const user = await db.User.findByPk(userId);
       user.blocked = block;
       await user.save();
@@ -38,37 +35,28 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
 
-db.sync();
+  await server.start();
 
-const app = express();
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-const corsOptions = {
-  origin: 'http://localhost:3000', // CHECK IF THIS IS CORRECT PORT
-  credentials: true,
-};
+  // Enable CORS for all routes
+  app.use(cors());
 
-app.use(cors(corsOptions));
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
-
-server.start().then(() => {
   server.applyMiddleware({ app });
 
-  require("./src/routes/user.routes.js")(express, app);
-  require("./src/routes/product.routes.js")(express, app);
-  require("./src/routes/review.routes.js")(express, app);
-  require("./src/routes/cart.routes.js")(express, app);
-  require("./src/routes/userFollows.routes.js")(express, app);
+  db.sync();
 
   const PORT = 4000;
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
     console.log(`GraphQL endpoint available at http://localhost:${PORT}${server.graphqlPath}`);
   });
-});
+}
+
+startApolloServer();
--- api/src/database/graphql/index.js
+++ api/src/database/graphql/index.js
@@ -1,4 +1,3 @@
-
 const { gql } = require("apollo-server-express");
 const { PubSub } = require("graphql-subscriptions");
 const db = require("../database");
@@ -6,43 +5,60 @@ const db = require("../database");
 const pubsub = new PubSub();
 
 const typeDefs = gql`
-    type User {
-        userId: ID!
-        username: String!
-        email: String!
-        blocked: Boolean!
-    }
-    
-    type Query {
-        users: [User]
-    }
-    
-    type Mutation {
-        blockUnblockUser(userId: ID!, block: Boolean!): User
-    }
-    
-    type Subscription {
-        userBlocked(userId: ID!): User
-    }
-    `;
+  type User {
+    userId: ID!
+    username: String!
+    email: String!
+    blocked: Boolean!
+  }
+  
+  type Query {
+    users: [User]
+  }
+  
+  type Mutation {
+    blockUnblockUser(userId: ID!, block: Boolean!): User
+  }
+  
+  type Subscription {
+    userBlocked(userId: ID!): User
+  }
+`;
+
 const resolvers = {
-    Query: {
-        users: async () => {
-            return await db.user.findAll();
-        },
+  Query: {
+    users: async () => {
+      try {
+        const users = await db.user.findAll();
+        return users;
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
+      }
     },
-    Mutation: {
-        blockUnblockUser: async (_, { userId, block }) => {
-            const user = await db.user.findByPk(userId);
-            user.blocked = block;
-            await user.save();
-            pubsub.publish("USER_BLOCKED", { userBlocked: user });
-            return user;
-        },
+  },
+  Mutation: {
+    blockUnblockUser: async (_, { userId, block }) => {
+      try {
+        const user = await db.user.findByPk(userId);
+        if (!user) {
+          throw new Error("User not found");
+        }
+        user.blocked = block;
+        await user.save();
+        pubsub.publish("USER_BLOCKED", { userBlocked: user });
+        return user;
+      } catch (error) {
+        console.error("Error blocking/unblocking user:", error);
+        throw error;
+      }
     },
-    Subscription: {
-        userBlocked: {
-            subscribe: () => pubsub.asyncIterator(["USER_BLOCKED"]),
-        },
+  },
+  Subscription: {
+    userBlocked: {
+      subscribe: (_, { userId }) => pubsub.asyncIterator(["USER_BLOCKED"]),
     },
+  },
 };
+
+module.exports = { typeDefs, resolvers };
